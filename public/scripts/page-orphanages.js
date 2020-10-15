c9094b10b82a524deadfcd0559f524a65fff0649
Anselmo <anselmo.margelo@outlook.com>
2020-10-14 23:57:13 -0300

    Terminado nado tela do mapa inicio da terceira tela

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,26 @@
+//create map
+const map = L.map('mapid').setView([-27.2193608, -49.6481216], 15);
+
+// create an add tilelayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+  iconUrl: './public/images/map-marker.svg',
+  iconSize: [58, 68],
+  iconAnchor: [29, 69],
+  popupAnchor: [170, 2],
+});
+
+//create popup overlay
+const popup = L.popup({
+  closeButton: false,
+  clasName: 'map-popup',
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>'
+);
+
+//create and add marker
+L.marker([-27.2193608, -49.6481216], { icon }).addTo(map).bindPopup(popup);
